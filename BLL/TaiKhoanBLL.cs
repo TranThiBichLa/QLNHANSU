4a69ab4bc681334fed10f9151d07c0e93dc9fa9b
TranThiBichLa <bichla12005@gmail.com>
2024-12-14 14:43:38 +0700

    Add files via upload

---
+++ BLL/TaiKhoanBLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+
+namespace BLL
+{
+    public class TaiKhoanBLL
+    {
+        TaiKhoanAccess tkAccess = new TaiKhoanAccess();
+        public string CheckLogin(TaiKhoan taikhoan)
+        {
+            //kiểm tra nhiệm vụ 
+            if (taikhoan.sTaiKhoan == "")
+            {
+                return "request_taikhoan";
+            }
+            if (taikhoan.sMatKhau == "")
+            {
+                return "request_password";
+            }
+            string info = tkAccess.CheckLogin(taikhoan);
+            return info;
+        }
+    }
+}
